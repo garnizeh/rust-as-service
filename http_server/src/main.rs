2af3bc19fd7c99f93598da58e4cc64ecc3b3625c
garnizeH <garnizeh.dev@gmail.com>
2024-07-04 14:17:57 -0300

    03 header extractor

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use axum::extract::Path;
 use axum::extract::Query;
+use axum::http::HeaderMap;
 use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
@@ -9,7 +10,8 @@ async fn main() {
     let app = Router::new()
         .route("/", get(handler))
         .route("/book/:id", get(path_extract))
-        .route("/book", get(query_extract));
+        .route("/book", get(query_extract))
+        .route("/header", get(header_extract));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
@@ -29,4 +31,8 @@ async fn path_extract(Path(id): Path<u32>) -> Html<String> {
 
 async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
     Html(format!("{params:#?}"))
+}
+
+async fn header_extract(headers: HeaderMap) -> Html<String> {
+    Html(format!("{headers:#?}"))
 }
