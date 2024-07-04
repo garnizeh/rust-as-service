87837a8cbf4ef03222e1863bf528fc9f27bd6ec9
garnizeH <garnizeh.dev@gmail.com>
2024-07-04 13:12:48 -0300

    02 added path extraction

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,8 +1,11 @@
+use axum::extract::Path;
 use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/book/:id", get(path_extract));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
@@ -14,4 +17,8 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>hello world</h1>")
+}
+
+async fn path_extract(Path(id): Path<u32>) -> Html<String> {
+    Html(format!("hello, {id}!"))
 }
